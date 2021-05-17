e4580ee8f39d58d7f5d8ec58a935e02c0a172bed
Vardaan Magon <vardaanmagon@hotmail.com>
2021-05-17 14:15:40 +0530

    Bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Photo from './Photo'
+import React, { useState, useEffect } from 'react';
+import Photo from './Photo';
 
-const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+//Storing the access key from .env file
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+//Storing the urls
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(0)
-  const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
+  //Async function to fetch api from Unsplash API
   const fetchImages = async () => {
-    setLoading(true)
-    let url
-    const urlPage = `&page=${page}`
-    const urlQuery = `&query=${query}`
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
     if (query) {
-      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
     } else {
-      url = `${mainUrl}${clientID}${urlPage}`
+      url = `${mainUrl}${clientID}${urlPage}`;
     }
     try {
-      const response = await fetch(url)
-      const data = await response.json()
+      const response = await fetch(url);
+      const data = await response.json();
       setPhotos((oldPhotos) => {
         if (query && page === 1) {
-          return data.results
+          return data.results;
         } else if (query) {
-          return [...oldPhotos, ...data.results]
+          return [...oldPhotos, ...data.results];
         } else {
-          return [...oldPhotos, ...data]
+          return [...oldPhotos, ...data];
         }
       })
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
   }
   useEffect(() => {
     fetchImages()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page])
-
+  //Event 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
       if (
@@ -50,17 +55,19 @@ function App() {
         document.body.scrollHeight - 2
       ) {
         setPage((oldPage) => {
-          return oldPage + 1
+          return oldPage + 1;
         })
       }
     })
-    return () => window.removeEventListener('scroll', event)
+    return () => window.removeEventListener('scroll', event);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setPage(1)
+    e.preventDefault();
+    setPage(1);
   }
+
+  //JSX code
   return (
     <main>
       <section className='search'>
